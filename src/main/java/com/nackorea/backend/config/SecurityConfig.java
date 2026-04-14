bf58650faa669aeb4fa6d5bf5891a3ca8cf5ca9a
nackorea <alexkr9173@gmail.com>
2026-04-14 18:02:49 +0900

    CORS 셋팅

--- src/main/java/com/nackorea/backend/config/SecurityConfig.java
+++ src/main/java/com/nackorea/backend/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
         cfg.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://localhost:8080",
-                "http://3.34.153.92:8080"
+                "http://127.0.0.1:8080"
         ));
         cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         cfg.setAllowedHeaders(List.of("*"));
